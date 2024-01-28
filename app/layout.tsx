d6f842c25efe70f40086053f27edf9856c7399e8
szne <me@szn.jp>
2024-01-29 01:45:00 +0900

    Feat: Footerを作成

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import "../src/reset.css";
 import "./globals.scss";
 
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 const noto = Noto_Sans_JP({ subsets: ["latin"] });
 
@@ -23,6 +24,7 @@ export default function RootLayout({
       <body className={noto.className}>
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
---
+++ components/Footer.tsx
@@ -0,0 +1,12 @@
+import "../src/root.scss";
+import styles from "./Footer.module.scss";
+
+const Header = () => {
+    return (
+        <footer className={styles.footer}>
+            <div className={styles.copyright}><span>&copy; 2024 - szne</span></div>
+        </footer>
+    );
+};
+
+export default Header;
