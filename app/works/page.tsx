27b4bd5c8e37a9fed42b6c0b6af0a258bed23be1
szne <me@szn.jp>
2024-02-06 23:23:23 +0900

    ✨ Feat: worksページを作成!

--- app/works/page.tsx
+++ app/works/page.tsx
@@ -1,13 +1,43 @@
+import Link from 'next/link'
+import Image from 'next/image';
+import imageSize from "image-size";
+
+import getWorks from '@/libs/getWorks'
+
+import styles from "./page.module.scss";
+
 export const metadata = {
     title: 'My works',
 };
 
-const Page = () => {
+export default async function Page() {
+    const works = await getWorks
     return (
-    <>
-        works
-    </>
+        <section className={styles.works}>
+            <h1>Works</h1>
+            <div className={styles.worklist}>
+                {works.map((post) => {
+                    const imagePath = post.frontmatter.image?.startsWith("/") ? post.frontmatter.image : ('/works/' + post.frontmatter.image);
+                    return (
+                        <article key={post.slug}>
+                            <Link href={`/works/${post.slug}`}>
+                                <div className={styles.imgwrap}>
+                                    <Image
+                                        alt={post.frontmatter.title + "のサムネイル"}
+                                        src={imagePath}
+                                        height={imageSize('public' + imagePath).height}
+                                        width={imageSize('public' + imagePath).width}
+                                    />
+                                </div>
+                                <div className={styles.postinfo}>
+                                    <p>{post.frontmatter.title}</p>
+                                    <span>{post.frontmatter.date}</span>
+                                </div>
+                            </Link>
+                        </article>
+                    );
+                })}
+            </div>
+        </section>
     );
 };
-
-export default Page;
