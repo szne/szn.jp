922aa6be5d260e88f6cd05caab009577b376e290
szne <me@szn.jp>
2024-02-08 15:53:30 +0900

    ✨ Feat: aboutページの作成

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,13 +1,21 @@
+import styles from "./page.module.scss";
+
 export const metadata = {
     title: 'About',
 };
 
-const Page = () => {
+export default async function Page() {
     return (
-    <>
-        About
-    </>
+        <>
+            <h1>About me</h1>
+            <section>
+                <h3>szne</h3>
+                <p>あんまり書くことないよな</p>
+                <p></p>
+            </section>
+            <section>
+                <h2>ロゴについて</h2>
+            </section>
+        </>
     );
 };
-
-export default Page;
